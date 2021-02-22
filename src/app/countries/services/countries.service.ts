c934b9d296ef99991e8dccfce1e78391244c3a08
crisgarm <crisgarm9@gmail.com>
2021-02-22 18:13:06 +0100

    Optimize http request

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/countries.interface';
@@ -9,18 +9,25 @@ import { Country } from '../interfaces/countries.interface';
 export class CountriesService {
   private apiUrl: string = 'https://restcountries.eu/rest/v2';
 
+  get httpParams() {
+    return new HttpParams().set(
+      'fields',
+      'name;capital;alpha2Code;flag;population'
+    );
+  }
+
   constructor(private http: HttpClient) {}
 
   //No quiero retornar la información dentro del servicio sino que quiero mandar la información a quien esté llamando a searchCountry
   //Ponemos en el tipado <Country[]> para que nos regrese un array de países y no solo uno
   searchCountry(query: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${query}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   searchCapital(query: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${query}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   getCountryByAlpha(id: string): Observable<Country> {
@@ -30,6 +37,6 @@ export class CountriesService {
 
   getCountryByRegion(region: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${region}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 }
