548a3cb1a8ecb30ca47ea3f529e22a928c5d8462
crisgarm <crisgarm9@gmail.com>
2021-02-22 16:21:45 +0100

    Add see country component and use switchMap operator

--- src/app/countries/pages/see-country/see-country.component.ts
+++ src/app/countries/pages/see-country/see-country.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-see-country',
   templateUrl: './see-country.component.html',
-  styleUrls: ['./see-country.component.css']
+  styleUrls: ['./see-country.component.css'],
 })
 export class SeeCountryComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountriesService
+  ) {}
 
   ngOnInit(): void {
+    //Accedemos al observable donde están los parámetros
+    //SwitchMap es un operador de rxjs que nos permite recibir un observable(params) y regresar otro (params.id)
+    this.activatedRoute.params
+      .pipe(
+        switchMap((params) => this.countryService.getCountryByAlpha(params.id))
+      )
+      .subscribe((resp) => {
+        console.log(resp);
+      });
   }
-
 }
+
+//Otra forma de hacerlo:
+//Para saber cuando cambia la URl, nos subscribimos al cambio de la URL mediante un observable y escuchamos ese cambio con activatedRoute
+// ngOnInit(): void {
+//   this.activatedRoute.params.subscribe((params) => {
+//     console.log(params.id);
+//     Hacemos otro observable para traer la información del país
+//     this.countryService.getCountryByAlpha(params.id).subscribe((country) => {
+//       console.log(country);
+//     });
+//   });
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -22,4 +22,9 @@ export class CountriesService {
     const url = `${this.apiUrl}/capital/${query}`;
     return this.http.get<Country[]>(url);
   }
+
+  getCountryByAlpha(id: string): Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
 }
